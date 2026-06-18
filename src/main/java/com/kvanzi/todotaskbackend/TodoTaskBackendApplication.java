76f4e9f227dbde5eba3ee0434c7380ef490d1936
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-06-18 14:31:32 +0300

    feat(app): add enable scheduling annotation

--- src/main/java/com/kvanzi/todotaskbackend/TodoTaskBackendApplication.java
+++ src/main/java/com/kvanzi/todotaskbackend/TodoTaskBackendApplication.java
@@ -3,9 +3,11 @@ package com.kvanzi.todotaskbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.modulith.Modulith;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Modulith
 @EnableJpaAuditing
+@EnableScheduling
 public class TodoTaskBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoTaskBackendApplication.class, args);

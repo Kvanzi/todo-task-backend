00d265843d4ce0ac361b9a4b7f59b7db3f71bb25
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-06-18 13:56:19 +0300

    feat(user): add public facade & mapping to user details

--- src/main/java/com/kvanzi/todotaskbackend/user/internal/mapper/UserMapper.java
+++ src/main/java/com/kvanzi/todotaskbackend/user/internal/mapper/UserMapper.java
@@ -1,12 +1,17 @@
 package com.kvanzi.todotaskbackend.user.internal.mapper;
 
+import com.kvanzi.todotaskbackend.shared.enumeration.Role;
+import com.kvanzi.todotaskbackend.shared.security.IdentifiableUserDetailsImpl;
 import com.kvanzi.todotaskbackend.user.api.dto.PrivateUserSummary;
 import com.kvanzi.todotaskbackend.user.api.dto.PublicUserSummary;
 import com.kvanzi.todotaskbackend.user.internal.dto.CreateUserRequest;
 import com.kvanzi.todotaskbackend.user.internal.entity.User;
+import org.jspecify.annotations.NonNull;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
@@ -16,4 +21,13 @@ public interface UserMapper {
     PublicUserSummary toPublicUserSummary(User userEntity);
 
     PrivateUserSummary toPrivateUserSummary(User userEntity);
+
+    @Mapping(source = "passwordHash", target = "password")
+    @Mapping(source = "email", target = "username")
+    @Mapping(source = "roles", target = "authorities")
+    IdentifiableUserDetailsImpl toIdentifiableUserDetails(User userEntity);
+
+    default @NonNull GrantedAuthority roleToAuthority(@NonNull Role role) {
+        return new SimpleGrantedAuthority("ROLE_" + role.name());
+    }
 }

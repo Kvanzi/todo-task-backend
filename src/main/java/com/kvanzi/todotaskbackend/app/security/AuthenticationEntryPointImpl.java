9eadff09680110bb592037e9228b85ed48d246c2
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-06-19 05:18:21 +0300

    feat(app): add authentication entry point

---
+++ src/main/java/com/kvanzi/todotaskbackend/app/security/AuthenticationEntryPointImpl.java
@@ -0,0 +1,53 @@
+package com.kvanzi.todotaskbackend.app.security;
+
+import com.kvanzi.todotaskbackend.auth.api.exception.JwtTokenException;
+import com.kvanzi.todotaskbackend.shared.api.HttpApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.json.JsonMapper;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+    private final JsonMapper mapper;
+
+    @Override
+    public void commence(@NonNull HttpServletRequest request, HttpServletResponse response,
+                         @NonNull AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setCharacterEncoding(StandardCharsets.UTF_8);
+        response.setHeader(HttpHeaders.CACHE_CONTROL, "no-store");
+        // same as cache-control but for HTTP/1.0 when cache-control for HTTP/1.1
+        response.setHeader(HttpHeaders.PRAGMA, "no-cache");
+
+        String message = "Authentication required to access this resource.";
+        boolean expected = authException instanceof JwtTokenException;
+
+        UUID errorId = null;
+        if (expected) {
+            message = authException.getMessage();
+        } else {
+            errorId = UUID.randomUUID();
+            log.error("[{}] Unexpected error occurred", errorId, authException);
+        }
+
+        response.getWriter().write(mapper.writeValueAsString(
+            HttpApiResponse.of(message, null, errorId)
+        ));
+        response.getWriter().flush();
+    }
+}

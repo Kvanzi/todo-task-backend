eeec35b3d43530673041fac67c2280287229185a
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-06-21 00:12:10 +0300

    feat(user): implement /api/users/me endpoint which returns current authenticated user

--- src/main/java/com/kvanzi/todotaskbackend/user/internal/controller/UserController.java
+++ src/main/java/com/kvanzi/todotaskbackend/user/internal/controller/UserController.java
@@ -1,18 +1,19 @@
 package com.kvanzi.todotaskbackend.user.internal.controller;
 
 import com.kvanzi.todotaskbackend.shared.api.HttpApiResponse;
+import com.kvanzi.todotaskbackend.shared.security.IdentifiableUserDetails;
 import com.kvanzi.todotaskbackend.user.api.dto.PrivateUserSummary;
+import com.kvanzi.todotaskbackend.user.api.exception.UserNotFoundException;
 import com.kvanzi.todotaskbackend.user.internal.dto.CreateUserRequest;
+import com.kvanzi.todotaskbackend.user.internal.mapper.UserMapper;
 import com.kvanzi.todotaskbackend.user.internal.service.UserService;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @PostMapping
     public ResponseEntity<@NonNull HttpApiResponse<@NonNull PrivateUserSummary, Void>> createUser(
@@ -36,6 +38,19 @@ public class UserController {
             .data(createdUser)
             .message("Successful registration")
             .location(location)
+            .build();
+    }
+
+
+    @GetMapping("/me")
+    public ResponseEntity<@NonNull HttpApiResponse<@NonNull PrivateUserSummary, Void>> getCurrentUser(
+        @NonNull @AuthenticationPrincipal IdentifiableUserDetails userDetails) {
+        PrivateUserSummary userSummary = userService.findUserById(userDetails.getId())
+            .map(userMapper::toPrivateUserSummary)
+            .orElseThrow(() -> new UserNotFoundException("Authenticated user not found."));
+
+        return HttpApiResponse.<PrivateUserSummary>ok()
+            .data(userSummary)
             .build();
     }
 }

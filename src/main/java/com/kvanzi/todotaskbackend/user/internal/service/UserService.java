935183bd3b3fc72a5bf3ec526bf9ce131f7ec383
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-06-21 04:54:28 +0300

    feat(user): implement update user method in user service

--- src/main/java/com/kvanzi/todotaskbackend/user/internal/service/UserService.java
+++ src/main/java/com/kvanzi/todotaskbackend/user/internal/service/UserService.java
@@ -4,12 +4,15 @@ import com.kvanzi.todotaskbackend.shared.enumeration.Role;
 import com.kvanzi.todotaskbackend.user.api.dto.PrivateUserSummary;
 import com.kvanzi.todotaskbackend.user.api.dto.PublicUserSummary;
 import com.kvanzi.todotaskbackend.user.api.exception.EmailTakenException;
+import com.kvanzi.todotaskbackend.user.api.exception.UserNotFoundException;
 import com.kvanzi.todotaskbackend.user.internal.dto.CreateUserRequest;
+import com.kvanzi.todotaskbackend.user.internal.dto.UpdateUserRequest;
 import com.kvanzi.todotaskbackend.user.internal.entity.User;
 import com.kvanzi.todotaskbackend.user.internal.mapper.UserMapper;
 import com.kvanzi.todotaskbackend.user.internal.repository.UserRepository;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
@@ -45,17 +48,7 @@ public class UserService {
         User user = userMapper.toEntity(request, passwordHash);
         user.addRole(Role.USER);
 
-        try {
-            return userMapper.toPrivateUserSummary(userRepository.saveAndFlush(user));
-        } catch (DataIntegrityViolationException e) {
-            if (e.getCause() instanceof ConstraintViolationException cve) {
-                String constraintName = cve.getConstraintName();
-                if (constraintName != null && constraintName.contains("email")) {
-                    throw new EmailTakenException(EMAIL_TAKEN_MESSAGE);
-                }
-            }
-            throw e;
-        }
+        return saveAndFlushSafely(() -> userMapper.toPrivateUserSummary(userRepository.saveAndFlush(user)));
     }
 
     public @NonNull Optional<User> findUserByEmailIgnoreCase(@NonNull String email) {
@@ -74,5 +67,45 @@ public class UserService {
     public @NonNull Page<@NonNull PublicUserSummary> getPublicUsers(@NonNull Pageable pageable) {
         return userRepository.findAll(pageable)
             .map(userMapper::toPublicUserSummary);
+    }
+
+    @Transactional
+    public @NonNull PrivateUserSummary updateUser(@NonNull UUID userId, @NonNull UpdateUserRequest request) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UserNotFoundException("User with id '%s' not found".formatted(userId)));
+
+        validateEmailUniquenessOrThrow(user.getEmail(), request.getEmail());
+        request.applyTo(user);
+
+        return saveAndFlushSafely(() -> userMapper.toPrivateUserSummary(userRepository.saveAndFlush(user)));
+    }
+
+    /**
+     * @throws EmailTakenException when email is taken by another user.
+     */
+    private void validateEmailUniquenessOrThrow(@NonNull String oldEmail, @NonNull String newEmail) {
+        if (oldEmail.equalsIgnoreCase(newEmail)) {
+            return;
+        }
+
+        if (!userRepository.existsByEmailIgnoreCase(newEmail)) {
+            return;
+        }
+
+        throw new EmailTakenException(EMAIL_TAKEN_MESSAGE);
+    }
+
+    private <T> T saveAndFlushSafely(Supplier<T> saveAction) {
+        try {
+            return saveAction.get();
+        } catch (DataIntegrityViolationException e) {
+            if (e.getCause() instanceof ConstraintViolationException cve) {
+                String constraintName = cve.getConstraintName();
+                if (constraintName != null && constraintName.contains("email")) {
+                    throw new EmailTakenException(EMAIL_TAKEN_MESSAGE);
+                }
+            }
+            throw e;
+        }
     }
 }

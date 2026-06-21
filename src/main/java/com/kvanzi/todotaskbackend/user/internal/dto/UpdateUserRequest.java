548c12401e10e1c2a7c3ddc1715bf3e6a4e5e209
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-06-21 03:40:11 +0300

    feat(user): create update user request DTO

---
+++ src/main/java/com/kvanzi/todotaskbackend/user/internal/dto/UpdateUserRequest.java
@@ -0,0 +1,23 @@
+package com.kvanzi.todotaskbackend.user.internal.dto;
+
+import com.kvanzi.todotaskbackend.shared.validation.UserEmail;
+import com.kvanzi.todotaskbackend.shared.validation.UserFirstName;
+import com.kvanzi.todotaskbackend.shared.validation.UserLastName;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
+public class UpdateUserRequest {
+    @UserFirstName
+    @NotNull(message = "First name cannot be null.")
+    @NonNull String firstName;
+
+    @UserLastName
+    @NotNull(message = "Last name cannot be null.")
+    @NonNull String lastName;
+
+    @UserEmail
+    @NotNull(message = "Email cannot be null.")
+    @NonNull String email;
+}

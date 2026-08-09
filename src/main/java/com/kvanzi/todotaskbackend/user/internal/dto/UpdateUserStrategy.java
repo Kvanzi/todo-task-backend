d52f734a81c3899d02e6dbc40cad9ea0aaafc393
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-08-09 22:49:18 +0300

    feat(user): add getRolesToAssign function to UpdateUserStrategy

--- src/main/java/com/kvanzi/todotaskbackend/user/internal/dto/UpdateUserStrategy.java
+++ src/main/java/com/kvanzi/todotaskbackend/user/internal/dto/UpdateUserStrategy.java
@@ -1,9 +1,17 @@
 package com.kvanzi.todotaskbackend.user.internal.dto;
 
+import com.kvanzi.todotaskbackend.shared.enumeration.Role;
 import com.kvanzi.todotaskbackend.user.internal.entity.User;
+import java.util.Optional;
+import java.util.Set;
+import org.jspecify.annotations.NonNull;
 
 public interface UpdateUserStrategy {
     void applyTo(User user);
 
     String getEmail();
+
+    default @NonNull Optional<Set<Role>> getRolesToAssign() {
+        return Optional.empty();
+    }
 }

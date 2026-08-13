d96fe769c1ae2e314cf3efa87dc0df28b9a5ed42
Nikita <180088125+Kvanzi@users.noreply.github.com>
2026-08-13 16:44:42 +0300

    feat(app): enable async event processing

--- src/main/java/com/kvanzi/todotaskbackend/TodoTaskBackendApplication.java
+++ src/main/java/com/kvanzi/todotaskbackend/TodoTaskBackendApplication.java
@@ -3,10 +3,12 @@ package com.kvanzi.todotaskbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.modulith.Modulith;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Modulith
 @EnableJpaAuditing
+@EnableAsync
 @EnableScheduling
 public class TodoTaskBackendApplication {
     public static void main(String[] args) {
